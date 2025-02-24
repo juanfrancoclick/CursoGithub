e119c6c9d7f3cf55bf21a193a7186d2f39cee39a
Renan Armando Valencia Zuluaga <ravz1811@gmail.com>
2025-02-24 14:45:54 -0500

    Se crea varieble empleado

--- CursoGithub/Clases/RenanValencia.cs
+++ CursoGithub/Clases/RenanValencia.cs
@@ -2,5 +2,17 @@
 {
     public class RenanValencia
     {
+        //crear una clase Empleado con los siguientes atributos: nombre, apellido, edad, salario, cargo, documento, telefono
+        public class Empleado
+        {
+            public string Nombre { get; set; }
+            public string Apellido { get; set; }
+            public int Edad { get; set; }
+            public double Salario { get; set; }
+            public string Cargo { get; set; }
+            public string Documento { get; set; }
+            public string Telefono { get; set; }
+        }
+
     }
 }
