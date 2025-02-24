4cc7a45f76f2d9b18e3a253105e5095992007e6e
JuanPa <juanphis2000@gmail.com>
2025-02-24 14:44:12 -0500

    Lista la Clase empreado JuaBet

--- CursoGithub/Clases/Juabet.cs
+++ CursoGithub/Clases/Juabet.cs
@@ -2,5 +2,19 @@
 {
     public class Juabet
     {
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Direccion { get; set; }
+        public string Telefono { get; set; }
+        public Juabet() { }
+        public Juabet(string nombre, string apellido, string direccion, string telefono)
+        {
+            Nombre = nombre;
+            Apellido = apellido;
+            Direccion = direccion;
+            Telefono = telefono;
+        }
+
+
     }
 }
