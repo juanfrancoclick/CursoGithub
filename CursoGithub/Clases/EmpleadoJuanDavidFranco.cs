cb1cdd9e9ae83b71607cfcb369888f7762ab0d5e
juanfrancoclick <juan.franco@clickstudio.co>
2025-02-24 19:42:32 +0000

    Clase empleado

--- CursoGithub/Clases/EmpleadoJuanDavidFranco.cs
+++ CursoGithub/Clases/EmpleadoJuanDavidFranco.cs
@@ -2,5 +2,18 @@
 {
     public class EmpleadoJuanDavidFranco
     {
+        //Crear clase empleado con variables nombre, apellido, edad, salario
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public int Edad { get; set; }
+        public double Salario { get; set; }
+        public EmpleadoJuanDavidFranco(string nombre, string apellido, int edad, double salario)
+        {
+            Nombre = nombre;
+            Apellido = apellido;
+            Edad = edad;
+            Salario = salario;
+        }
+        
     }
 }
